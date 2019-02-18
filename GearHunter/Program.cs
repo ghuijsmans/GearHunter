2f05f8b9a80bceb889f65a1b6d1011da31836835
Gijs Huijsmans <huijsmans.gijs@gmail.com>
2019-02-18 16:27:13 +0100

    ee2rrrasdasdas

--- GearHunter/Program.cs
+++ GearHunter/Program.cs
@@ -16,28 +16,35 @@ namespace GearHunter
 {
     class Program
     {
-            private static void DriverManager(IWebDriver driver)
-            {
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
-            }
-            
-            private static void SendEmail()
-            {
-            //BEGIN MAIL DINGES
+        private static void DriverManager(IWebDriver driver)
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+        }
 
-            //string credentialGebruikersnaam = "csharpemailer@gmail.com";
-            //string credentialWachtwoord = "Watisdeze1213";
-            //string smtp = "smtp.gmail.com";
-            //string inhoudMail = "Er is een nieuwe advertentie geplaatst!\n De link naar de advertentie is: dinges";
+        private static string GetEmail()
+        {
+            SqlConnection conn = new SqlConnection("Data Source = DESKTOP-MOM6ULT; Initial Catalog = GearHunter; Integrated Security = True");
+            conn.Open();
 
+            SqlCommand command = new SqlCommand("SELECT Email FROM TargetEmail WHERE Id = 1", conn);
+
+            string TargetEmail = (string)command.ExecuteScalar();
+            Console.WriteLine("Resultaten worden verstuurd naar: " + TargetEmail);
+
+            conn.Close();
+            return TargetEmail;
+        }
+
+        private static void SendEmail(string TargetEmail, string Keyword)
+        {
             try
             {
                 var mail = new MailMessage();
                 var smtpServer = new SmtpClient("smtp.gmail.com", 587);
-                mail.From = new MailAddress("csharpemailer@gmail.com", "Tester.Email");
-                mail.To.Add("csharpemailer@gmail.com");
-                mail.Subject = "EMAIL ONDERWERP";
-                mail.Body = "123456789 10.";
+                mail.From = new MailAddress("csharpemailer@gmail.com", "GearHunter");
+                mail.To.Add(TargetEmail);
+                mail.Subject = "!! " + Keyword;
+                mail.Body = "Er is mogelijk een of meer nieuwe advertentie(s) aangetroffen onder de zoekterm: " + Keyword + ".";
                 smtpServer.Credentials = new NetworkCredential("csharpemailer@gmail.com", "Watisdeze1213");
                 smtpServer.EnableSsl = true;
                 smtpServer.Send(mail);
@@ -49,84 +56,133 @@ namespace GearHunter
             }
         }
 
-            private static string SelectKeyWord(int CurrentKeyword)
-            {
-                SqlConnection conn = new SqlConnection("Data Source = DESKTOP-MOM6ULT; Initial Catalog = GearHunter; Integrated Security = True");
-                conn.Open();
+        private static void Sleepysleep()
+        {
+            Thread.Sleep(60000);
+        }
 
-                SqlCommand command = new SqlCommand("SELECT Keyword FROM Keywords WHERE Id = '" + CurrentKeyword + "'", conn);
+        private static string SelectKeyWord(int CurrentKeyword)
+        {
+            SqlConnection conn = new SqlConnection("Data Source = DESKTOP-MOM6ULT; Initial Catalog = GearHunter; Integrated Security = True");
+            conn.Open();
+
+            SqlCommand command = new SqlCommand("SELECT Keyword FROM Keywords WHERE Id = '" + CurrentKeyword + "'", conn);
             
-                string SearchWord = (string)command.ExecuteScalar();
-                Console.WriteLine("Huidige Zoekterm: " + SearchWord);    
+            string SearchWord = (string)command.ExecuteScalar();
+            Console.WriteLine("Huidige Zoekterm: " + SearchWord);    
 
-                conn.Close();
-                return SearchWord;
-            }
-
-            private static int SelectKeyWordAmount()
+            conn.Close();
+            return SearchWord;
+        }
+        
+        private static List<string> SelectKeywords()
+        {
+            List<string> list_keywords = new List<string>();
+            SqlConnection conn = new SqlConnection("Data Source = DESKTOP-MOM6ULT; Initial Catalog = GearHunter; Integrated Security = True");
+            SqlCommand command = new SqlCommand("SELECT Keyword FROM Keywords", conn);
+            try
             {
-                SqlConnection conn = new SqlConnection("Data Source = DESKTOP-MOM6ULT; Initial Catalog = GearHunter; Integrated Security = True");
                 conn.Open();
-
-                SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Keywords", conn);
-                int Count = (int)command.ExecuteScalar();
-                Console.WriteLine("Aantal Zoektermen: " + Count);  
-
-                conn.Close();
-                return Count;
-            }
-            
-            static void Main(string[] args)
-            {
-                int KeywordsAmount = SelectKeyWordAmount();
-                bool Running = true;
-                while (Running == true)
+                SqlDataReader dr = command.ExecuteReader();
+                while (dr.Read())
                 {
-                    int CurrentKeyword = 1;
-                    
-                    while (CurrentKeyword <= KeywordsAmount)
-                        {
-                        string SearchWord = SelectKeyWord(CurrentKeyword);
-
-                        IWebDriver driver = new ChromeDriver();
-                        driver.Manage().Window.Maximize();
-
-                        //open chrome web driver
-                        driver.Navigate().GoToUrl("https://reverb.com");
-                        DriverManager(driver);
-                        IWebElement element = driver.FindElement(By.ClassName("site-search__controls__input"));
-
-                        //Enter what you want to search for
-
-                        element.SendKeys(SearchWord);
-                        element.SendKeys(Keys.Enter);
-                        DriverManager(driver);
-
-                        //Switching to react modal
-                    
-                        driver.SwitchTo().ActiveElement();
-                        IWebElement element1 = driver.FindElement(By.Id("user[first_name]"));
-                        element1.SendKeys(Keys.Escape);
-
-                        //Getting the Number of listings
-                        
-                        string listno = driver.FindElement(By.XPath("/html/body/div[3]/section/div[2]/div/div/div/div[2]/div/div[2]/nav/div[1]/h2/div")).Text.ToString();
-                        listno.Replace(" Listings", "");
-                        //int listnoint = Convert.ToInt32(listno);
-                        Console.WriteLine("Klaar met zoekwoord no#: "+ CurrentKeyword.ToString());
-                        Console.WriteLine("Aantal aangetroffen listings: " + listno.ToString());
-                    
-                        CurrentKeyword = CurrentKeyword + 1;
-                        Console.WriteLine(CurrentKeyword);
-                        driver.Close();
-
-                        //if(CurrentKeyword == KeywordsAmount)
-                    //{
-                        //Running = false;
-                    //}
+                    int i = 0;
+                    list_keywords.Add(dr.GetString(i));
+                    i = i + 1;
                 }
+                conn.Close();
+                return list_keywords;
             }
-         }
+            catch
+            {
+                string foutmelding = "foutmelding in de catch van de lijst ophalen.";
+                List<string> melding = new List<string>();
+                melding.Add(foutmelding);
+                return melding;
+                /* error */
+            }
+        } 
+        private static int SelectKeyWordAmount()
+        {
+            SqlConnection conn = new SqlConnection("Data Source = DESKTOP-MOM6ULT; Initial Catalog = GearHunter; Integrated Security = True");
+            conn.Open();
+
+            SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Keywords", conn);
+            int Count = (int)command.ExecuteScalar();
+            Console.WriteLine("Aantal Zoektermen: " + Count);  
+
+            conn.Close();
+            return Count;
+        }
+            
+        static void Main(string[] args)
+        {
+            int KeywordsAmount = SelectKeyWordAmount();
+            bool Running = true;
+            string TargetEmail = GetEmail();
+
+            List<string> Keywords = new List<string>();
+            Keywords = SelectKeywords();
+
+
+            while (Running == true)
+            {   
+                foreach(string Keyword in Keywords)
+                {
+                    Console.WriteLine("Zoekt nu naar: " + Keyword);
+                    IWebDriver driver = new ChromeDriver();
+                    driver.Manage().Window.Maximize();
+
+                    //open chrome web driver
+                    driver.Navigate().GoToUrl("https://reverb.com");
+                    DriverManager(driver);
+                    IWebElement element = driver.FindElement(By.ClassName("site-search__controls__input"));
+
+                    //Enter what you want to search for
+
+                    element.SendKeys(Keyword);
+                    element.SendKeys(Keys.Enter);
+                    DriverManager(driver);
+
+                    //Switching to react modal
+
+                    driver.SwitchTo().ActiveElement();
+                    IWebElement element1 = driver.FindElement(By.Id("user[first_name]"));
+                    element1.SendKeys(Keys.Escape);
+
+                    //Getting the Number of listings
+
+                    try
+                    {
+                        string listno = driver.FindElement(By.XPath("/html/body/div[3]/section/div[2]/div/div/div/div[2]/div/div[2]/nav/div[1]/h2/div")).Text.ToString();
+                        Console.WriteLine("Klaar met zoekwoord no#: " + Keyword);
+                        Console.WriteLine("Aantal aangetroffen listings: " + listno.ToString());
+                        driver.Close();
+                        //listno.Replace(" Listings", "");
+                        //int listnoint = Convert.ToInt32(listno);
+                        //SendEmail(TargetEmail, Keyword);
+                        //Console.WriteLine(listnoint);
+                        Console.WriteLine("");
+                    }
+                    catch
+                    {
+                        string listno = driver.FindElement(By.XPath("/html/body/div[3]/section/div[2]/div/div/div/div[2]/div/div[2]/nav/section/div/h2")).Text.ToString();
+                        Console.WriteLine("Klaar met zoekwoord no#: " + Keyword);
+                        Console.WriteLine("Aantal aangetroffen listings: " + listno.ToString());
+                        driver.Close();
+                        //listno.Replace(" Listings", "");
+                        //int listnoint = Convert.ToInt32(listno);
+                        //SendEmail(TargetEmail, Keyword);
+                        //Console.WriteLine(listnoint);
+                        Console.WriteLine("");
+                    }
+                    
+                    
+                }
+            }  
+        }
     }
 }
+
+
 
