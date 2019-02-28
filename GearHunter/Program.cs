f010d5205dee1984b416233dd0525d8faa61d430
Redmar Schets <1752073schets@zuyd.nl>
2019-02-28 22:22:26 +0100

    hadehooow

--- GearHunter/Program.cs
+++ GearHunter/Program.cs
@@ -23,15 +23,15 @@ namespace GearHunter
 
         private static string GetEmail()
         {
-            SqlConnection conn = new SqlConnection("Data Source = DESKTOP-MOM6ULT; Initial Catalog = GearHunter; Integrated Security = True");
-            conn.Open();
-
+            SqlConnection conn = new SqlConnection("Data Source = localhost; Initial Catalog = GearHunter; Integrated Security = True");
             SqlCommand command = new SqlCommand("SELECT Email FROM TargetEmail WHERE Id = 1", conn);
 
+            conn.Open();
             string TargetEmail = (string)command.ExecuteScalar();
+            conn.Close();
+
             Console.WriteLine("Resultaten worden verstuurd naar: " + TargetEmail);
 
-            conn.Close();
             return TargetEmail;
         }
 
@@ -43,8 +43,8 @@ namespace GearHunter
                 var smtpServer = new SmtpClient("smtp.gmail.com", 587);
                 mail.From = new MailAddress("csharpemailer@gmail.com", "GearHunter");
                 mail.To.Add(TargetEmail);
-                mail.Subject = "!! " + Keyword;
-                mail.Body = "Er is mogelijk een of meer nieuwe advertentie(s) aangetroffen onder de zoekterm: " + Keyword + ".";
+                mail.Subject = "Mogelijke nieuwe advertentie!";
+                mail.Body = "Er is mogelijk een of meer nieuwe advertentie(s) aangetroffen onder de url: \n" + Keyword + ".";
                 smtpServer.Credentials = new NetworkCredential("csharpemailer@gmail.com", "Watisdeze1213");
                 smtpServer.EnableSsl = true;
                 smtpServer.Send(mail);
@@ -55,31 +55,12 @@ namespace GearHunter
                 Console.WriteLine(ex);
             }
         }
-
-        private static void Sleepysleep()
-        {
-            Thread.Sleep(60000);
-        }
-
-        private static string SelectKeyWord(int CurrentKeyword)
-        {
-            SqlConnection conn = new SqlConnection("Data Source = DESKTOP-MOM6ULT; Initial Catalog = GearHunter; Integrated Security = True");
-            conn.Open();
-
-            SqlCommand command = new SqlCommand("SELECT Keyword FROM Keywords WHERE Id = '" + CurrentKeyword + "'", conn);
-            
-            string SearchWord = (string)command.ExecuteScalar();
-            Console.WriteLine("Huidige Zoekterm: " + SearchWord);    
-
-            conn.Close();
-            return SearchWord;
-        }
         
         private static List<string> SelectKeywords()
         {
             List<string> list_keywords = new List<string>();
-            SqlConnection conn = new SqlConnection("Data Source = DESKTOP-MOM6ULT; Initial Catalog = GearHunter; Integrated Security = True");
-            SqlCommand command = new SqlCommand("SELECT Keyword FROM Keywords", conn);
+            SqlConnection conn = new SqlConnection("Data Source = localhost; Initial Catalog = GearHunter; Integrated Security = True");
+            SqlCommand command = new SqlCommand("SELECT KeywordURL FROM Keywords", conn);
             try
             {
                 conn.Open();
@@ -95,16 +76,16 @@ namespace GearHunter
             }
             catch
             {
-                string foutmelding = "foutmelding in de catch van de lijst ophalen.";
+                string foutmelding = "Foutmelding in de catch van de lijst ophalen.";
                 List<string> melding = new List<string>();
                 melding.Add(foutmelding);
                 return melding;
-                /* error */
             }
         } 
+
         private static int SelectKeyWordAmount()
         {
-            SqlConnection conn = new SqlConnection("Data Source = DESKTOP-MOM6ULT; Initial Catalog = GearHunter; Integrated Security = True");
+            SqlConnection conn = new SqlConnection("Data Source = localhost; Initial Catalog = GearHunter; Integrated Security = True");
             conn.Open();
 
             SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Keywords", conn);
@@ -114,72 +95,211 @@ namespace GearHunter
             conn.Close();
             return Count;
         }
+
+        private static int TrimStringConvertToInt(string tekst)
+        {
+            string trimmedString = tekst.Trim(new char[] { ' ', 'l', 'i', 's', 't', 'n', 'g' });
+            int listingInt = Int32.Parse(trimmedString);
+            return listingInt;
+        }
+
+        private static bool CompareListingsWithDatabase(string keyword, int listnoInt)
+        {
+            string conString = "Data Source = localhost; Initial Catalog = GearHunter; Integrated Security = True";
+            string sqlQuery1 = "SELECT LastAmount FROM Keywords WHERE Keyword = '" +keyword+ "'";
+            string sqlQuery2 = "UPDATE Keywords SET LastAmount = '" +listnoInt+ "' WHERE Keyword = '" +keyword+ "'";
+
+            SqlConnection conn = new SqlConnection(conString);
+            SqlCommand cmd1 = new SqlCommand(sqlQuery1, conn);
+            SqlCommand cmd2 = new SqlCommand(sqlQuery2, conn);
+
+            conn.Open();
+            int AmountInDatabase = Convert.ToInt32(cmd1.ExecuteScalar());
+            conn.Close();
+
+            if (AmountInDatabase < listnoInt)
+            {
+                conn.Open();
+                cmd2.ExecuteNonQuery();
+                conn.Close();
+                return true;
+            }
+            else if (AmountInDatabase > listnoInt)
+            {
+                conn.Open();
+                cmd2.ExecuteNonQuery();
+                conn.Close();
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static int GetIntervalMinMain()
+        {
+            string conString = "Data Source = localhost; Initial Catalog = GearHunter; Integrated Security = True";
+            string sqlQuery = "SELECT IntervalMin FROM SetTimeInterval WHERE TimeIntervalFor = 'MainProgram'";
+
+            SqlConnection conn = new SqlConnection(conString);
+            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
             
+            conn.Open();
+            int intervalMin = Convert.ToInt32(cmd.ExecuteScalar());
+            conn.Close();
+
+            return intervalMin;
+        }
+
+        private static int GetIntervalMaxMain()
+        {
+            string conString = "Data Source = localhost; Initial Catalog = GearHunter; Integrated Security = True";
+            string sqlQuery = "SELECT IntervalMax FROM SetTimeInterval WHERE TimeIntervalFor = 'MainProgram'";
+
+            SqlConnection conn = new SqlConnection(conString);
+            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+
+            conn.Open();
+            int intervalMax = Convert.ToInt32(cmd.ExecuteScalar());
+            conn.Close();
+
+            return intervalMax;
+        }
+
+        private static int GetIntervalMinBetweenSearches()
+        {
+            string conString = "Data Source = localhost; Initial Catalog = GearHunter; Integrated Security = True";
+            string sqlQuery = "SELECT IntervalMin FROM SetTimeInterval WHERE TimeIntervalFor = 'BetweenSearches'";
+
+            SqlConnection conn = new SqlConnection(conString);
+            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+
+            conn.Open();
+            int intervalMin = Convert.ToInt32(cmd.ExecuteScalar());
+            conn.Close();
+
+            return intervalMin;
+        }
+
+        private static int GetIntervalMaxBetweenSearches()
+        {
+            string conString = "Data Source = localhost; Initial Catalog = GearHunter; Integrated Security = True";
+            string sqlQuery = "SELECT IntervalMax FROM SetTimeInterval WHERE TimeIntervalFor = 'BetweenSearches'";
+
+            SqlConnection conn = new SqlConnection(conString);
+            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+
+            conn.Open();
+            int intervalMax = Convert.ToInt32(cmd.ExecuteScalar());
+            conn.Close();
+
+            return intervalMax;
+        }
+
         static void Main(string[] args)
         {
             int KeywordsAmount = SelectKeyWordAmount();
             bool Running = true;
+
             string TargetEmail = GetEmail();
 
-            List<string> Keywords = new List<string>();
-            Keywords = SelectKeywords();
+            int intervalMinMainProgram = GetIntervalMinMain()*1000;
+            int intervalMaxMainProgram = GetIntervalMaxMain()*1000;
+            int intervalMinBetweenSearches = GetIntervalMinBetweenSearches()*1000;
+            int intervalMaxBetweenSearches = GetIntervalMaxBetweenSearches()*1000;
 
+            List<string> KeywordsUrl = new List<string>();
+            KeywordsUrl = SelectKeywords();
+
+            List<string> xPaden = new List<string>();
+            xPaden.Add("/html/body/div[3]/section/div[2]/div/div/div/div[2]/div/div[2]/nav/div[1]/h2/div");
+            xPaden.Add("/html/body/div[3]/section/div[2]/div/div/div/div[2]/div/div[2]/nav/section/div/h2");
+            xPaden.Add("/html/body/div/section/div[2]/div/div/div/div[2]/div/div[2]/nav/section/div/h2");
 
             while (Running == true)
-            {   
-                foreach(string Keyword in Keywords)
+            {
+                foreach(string Keyword in KeywordsUrl)
                 {
                     Console.WriteLine("Zoekt nu naar: " + Keyword);
                     IWebDriver driver = new ChromeDriver();
                     driver.Manage().Window.Maximize();
 
                     //open chrome web driver
-                    driver.Navigate().GoToUrl("https://reverb.com");
+                    driver.Navigate().GoToUrl(Keyword);
                     DriverManager(driver);
-                    IWebElement element = driver.FindElement(By.ClassName("site-search__controls__input"));
-
-                    //Enter what you want to search for
-
-                    element.SendKeys(Keyword);
-                    element.SendKeys(Keys.Enter);
-                    DriverManager(driver);
+                    //IWebElement element = driver.FindElement(By.ClassName("site-search__controls__input"));
 
                     //Switching to react modal
-
                     driver.SwitchTo().ActiveElement();
-                    IWebElement element1 = driver.FindElement(By.Id("user[first_name]"));
-                    element1.SendKeys(Keys.Escape);
+                    try
+                    {
+                        IWebElement element1 = driver.FindElement(By.Id("user[first_name]"));
+                        element1.SendKeys(Keys.Escape);
+                        Console.WriteLine("Popup closed.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
 
                     //Getting the Number of listings
 
-                    try
+                    // Ik had gehoopt dat dit zou werken, dus dat zodra je een hit hebt op regel 263 dat hij de foreach loop afbreekt omdat run true wordt.
+                    // Echter blijft hij alle paden in xPaden aflopen totdat die ze allemaal gehad heeft, ookal heeft hij het eerste pad een hit. 
+                    // Wellicht weet jij hier dan een betere oplossing voor, ik had gehoopt dat die whileloop 'overheerst' over die foreach zeg maar. 
+                    // Ook hebben we die paden nu hardcoded erinstaan (regel 215), misschien iets om die ook maar 1x uit de database te halen. 
+                    // Maar dat is mss ook iets voor later. 
+
+                    string listno = "";
+                    bool run = true;
+                    while (run)
                     {
-                        string listno = driver.FindElement(By.XPath("/html/body/div[3]/section/div[2]/div/div/div/div[2]/div/div[2]/nav/div[1]/h2/div")).Text.ToString();
-                        Console.WriteLine("Klaar met zoekwoord no#: " + Keyword);
-                        Console.WriteLine("Aantal aangetroffen listings: " + listno.ToString());
-                        driver.Close();
-                        //listno.Replace(" Listings", "");
-                        //int listnoint = Convert.ToInt32(listno);
-                        //SendEmail(TargetEmail, Keyword);
-                        //Console.WriteLine(listnoint);
-                        Console.WriteLine("");
+                        foreach(string pad in xPaden)
+                        {
+                            try
+                            {
+                                listno = driver.FindElement(By.XPath(pad)).Text.ToString();
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Leeg");
+                                listno = "Leeg";
+                            }
+                            bool checkListno = listno.Contains("listings");
+                            if (checkListno)
+                            {
+                                run = false;
+                            }
+                        }
                     }
-                    catch
+
+                    Console.WriteLine("Klaar met zoekwoord no#: " + Keyword);
+                    Console.WriteLine("Aantal aangetroffen listings: " + listno.ToString());
+                    driver.Close();
+
+                    int listnoInt = TrimStringConvertToInt(listno);
+                    //DEZE NOG AANPASSEN
+                    bool sendMail = CompareListingsWithDatabase(Keyword, listnoInt);
+                    if (sendMail)
                     {
-                        string listno = driver.FindElement(By.XPath("/html/body/div[3]/section/div[2]/div/div/div/div[2]/div/div[2]/nav/section/div/h2")).Text.ToString();
-                        Console.WriteLine("Klaar met zoekwoord no#: " + Keyword);
-                        Console.WriteLine("Aantal aangetroffen listings: " + listno.ToString());
-                        driver.Close();
-                        //listno.Replace(" Listings", "");
-                        //int listnoint = Convert.ToInt32(listno);
-                        //SendEmail(TargetEmail, Keyword);
-                        //Console.WriteLine(listnoint);
-                        Console.WriteLine("");
+                        SendEmail(TargetEmail, Keyword);
                     }
+
+                    // hier sleep BetweenSearches
+                    //Random r = new Random();
+                    //int intervalBetweenSearches = r.Next(intervalMinBetweenSearches, intervalMaxBetweenSearches);
+                    //Thread.Sleep(intervalBetweenSearches);
                     
-                    
+                    Console.WriteLine("");
                 }
-            }  
+
+                // hier sleep MainProgram
+                //Random r2 = new Random();
+                //int intervalMainProgram = r2.Next(intervalMinMainProgram, intervalMaxMainProgram);
+                SendEmail("csharpemailer@gmail.com", "Still going strong!");
+                //Thread.Sleep(intervalMainProgram);
+            }
         }
     }
 }
